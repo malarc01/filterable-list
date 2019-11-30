d8a3c4bdf64c675ff5a348cf922fd066802e5c86
malarc01 <45378877+malarc01@users.noreply.github.com>
2019-11-30 16:21:51 -0500

    add row& column css flex & searchColumn2

--- main.js
+++ main.js
@@ -1,18 +1,43 @@
 const filterInput = document.querySelector('#filterInput');
-const names = [...document.querySelectorAll('.letter-group__name a')];
+const names = [ ...document.querySelectorAll('.letter-group__name a') ];
 const form = document.querySelector('.search-form');
 
 filterInput.focus();
 
 function filterName() {
-    const value = this.value.toUpperCase();
-    names.forEach(name =>
-        name.textContent.toUpperCase().includes(value) ? name.classList.remove('hide') : name.classList.add('hide')
-    );
+	const value = this.value.toUpperCase();
+	names.forEach(
+		(name) =>
+			name.textContent.toUpperCase().includes(value) ? name.classList.remove('hide') : name.classList.add('hide')
+	);
 }
 
 filterInput.addEventListener('change', filterName);
 filterInput.addEventListener('keyup', filterName);
 form.addEventListener('submit', function(e) {
-    e.preventDefault();
+	e.preventDefault();
+});
+// search column two
+const filterInputTwo = document.querySelector('#filterInput-two');
+const allVegtables = [ ...document.querySelectorAll('.vegtable a') ];
+
+const formTwo = document.querySelector('.search-form-two');
+
+filterInputTwo.focus();
+
+function filterVegtable() {
+	const value = this.value.toUpperCase();
+	allVegtables.forEach(
+		(vegtable) =>
+			vegtable.textContent.toUpperCase().includes(value)
+				? vegtable.classList.remove('hide')
+				: vegtable.classList.add('hide')
+	);
+}
+filterInputTwo.addEventListener('change', filterVegtable);
+
+filterInputTwo.addEventListener('keyup', filterVegtable);
+
+formTwo.addEventListener('submit', function(e) {
+	e.preventDefault();
 });
